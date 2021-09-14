f880efff5877ec39ac0454863e1fe2ae687b4835
Samuel silva <samuel@debian.debian.com.br>
2021-09-14 15:44:31 -0300

    static

--- roda_case/settings.py
+++ roda_case/settings.py
@@ -121,11 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATIC_URL = "/static/"
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
