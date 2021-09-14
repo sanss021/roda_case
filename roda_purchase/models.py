48b556191a478798c779f6b04d79e167b88cb388
Samuel silva <samuel@debian.debian.com.br>
2021-09-14 17:21:51 -0300

    deploy

--- roda_purchase/models.py
+++ roda_purchase/models.py
@@ -3,9 +3,17 @@ from django.db import models
 
 # Create your models here.
 
+class Payment(models.Model):
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.type
+
 class Product(models.Model):
-    item= models.CharField(max_length=200)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    item= models.CharField(max_length=100)
     price= models.DecimalField(max_digits=5, decimal_places=2)
+
     
 
     def __str__(self):
@@ -16,6 +24,8 @@ class Purchase(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     client = models.CharField(max_length=100)
     products = models.ManyToManyField('Product')
+    payment= models.ForeignKey(Payment,on_delete=models.DO_NOTHING, blank=True)
+    
 
 
     def __str__(self):
